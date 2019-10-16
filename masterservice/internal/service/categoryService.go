53e0ca7ab8e7475d3e946e7978c6c7c3e0b62c88
satvasu <sathishkumar64@gmail.com>
2019-10-16 15:16:00 +0530

    Updated code & working code

--- masterservice/internal/service/categoryService.go
+++ masterservice/internal/service/categoryService.go
@@ -9,14 +9,15 @@ import (
 )
 //RegisterCategory is used to parse CSV and insert data into mongodb.
 func RegisterCategory(ctx context.Context,database *durable.Database,csvFileName string){
-
-	var category *mongo.Collection
-	var categoryObj []model.Category
+	var (
+		category *mongo.Collection
+		categoryObj []model.Category
+		categoryInterface []interface{}
+		)
 	category = database.Db.Database("schoolservice").Collection("category")
 	withExtFileName:=	csvFileName +".csv"
 	reader:=bulkupload.ParseCSV(ctx, withExtFileName)
 	categoryObj = bulkupload.Category(reader)
-	var categoryInterface []interface{}
 	for _, p := range categoryObj {
 		categoryInterface = append(categoryInterface, p)
 	}
@@ -26,13 +27,15 @@ func RegisterCategory(ctx context.Context,database *durable.Database,csvFileName
 
 //RegisterSubCategory is used to parse CSV and insert data into mongodb.
 func RegisterSubCategory(ctx context.Context,database *durable.Database,csvFileName string){
-	var subCategory *mongo.Collection
-	var categoryObj []model.SubCategory
+	var (
+		subCategory *mongo.Collection
+		categoryObj []model.SubCategory
+		subCategoryInterface []interface{}
+	)
 	subCategory = database.Db.Database("schoolservice").Collection("subcategory")
 	withExtFileName:=	csvFileName +".csv"
 	reader:=bulkupload.ParseCSV(ctx, withExtFileName)
 	categoryObj = bulkupload.SubCategory(reader)
-	var subCategoryInterface []interface{}
 	for _, p := range categoryObj {
 		subCategoryInterface = append(subCategoryInterface, p)
 	}
